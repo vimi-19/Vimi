ffebbf50452ea3b829bdae1fffe4b4770d5b024a
Bruno Vicente <103516597+BrunoVSSilva@users.noreply.github.com>
2022-11-27 13:14:30 -0300

    Entrega da página marketplace feita pela karine

---
+++ Home Marketplace/carrosel.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 3,
+    freeMode: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+});
