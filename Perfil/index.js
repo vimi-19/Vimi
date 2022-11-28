a7466c80df5d171a1544e89370cb5771c33a559e
Bruno Vicente <103516597+BrunoVSSilva@users.noreply.github.com>
2022-11-28 17:41:35 -0300

    Add files via upload

--- Perfil/index.js
+++ Perfil/index.js
@@ -9,7 +9,7 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
-var x = window.matchMedia("(max-width: 1440px)")
+var x = window.matchMedia("(max-width: 1518px)")
 myFunction(x) // Call listener function at run time
 x.addListener(myFunction) // Attach listener function on state changes
 
@@ -43,6 +43,58 @@ y.addListener(myFunction) // Attach listener function on state changes
 
 function myFunction(y) {
     if (y.matches) { // If media query matches
+        swiper = new Swiper(".mySwiper", {
+            slidesPerView: 2,
+            freeMode: true,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        })
+    } else {
+        swiper = new Swiper(".mySwiper", {
+            slidesPerView: 3,
+            freeMode: true,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        })
+    }
+}
+
+var z = window.matchMedia("(max-width: 894px)")
+myFunction(z) // Call listener function at run time
+z.addListener(myFunction) // Attach listener function on state changes
+
+function myFunction(z) {
+    if (z.matches) { // If media query matches
+        swiper = new Swiper(".mySwiper", {
+            slidesPerView: 1,
+            freeMode: true,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        })
+    } else {
+        swiper = new Swiper(".mySwiper", {
+            slidesPerView: 2,
+            freeMode: true,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        })
+    }
+}
+
+var h = window.matchMedia("(max-width: 768px)")
+myFunction(h) // Call listener function at run time
+h.addListener(myFunction) // Attach listener function on state changes
+
+function myFunction(h) {
+    if (h.matches) { // If media query matches
         swiper = new Swiper(".mySwiper", {
             slidesPerView: 2,
             freeMode: true,
